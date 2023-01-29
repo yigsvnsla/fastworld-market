8c05e4947dc2b9d5195d6d4f86db0437e03604a3
yigsvnsla <yigsvnsla@gmail.com>
2023-01-29 09:47:24 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'fastworld-market',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
